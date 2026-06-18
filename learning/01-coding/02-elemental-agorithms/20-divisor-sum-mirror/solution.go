9f58de01b9f2585a2f4592101d9df842784f0451
Cristian Albu <albucristian95p@gmail.com>
2026-06-18 10:16:27 +0300

    last elemental algo problem

--- learning/01-coding/02-elemental-agorithms/20-divisor-sum-mirror/solution.go
+++ learning/01-coding/02-elemental-agorithms/20-divisor-sum-mirror/solution.go
@@ -1,6 +1,46 @@
 package divisorsummirror
 
-func Solution(n int) int {
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
 
-	return 0
+func Solution(nums string) int {
+	data := strings.Split(nums, " ")
+	sum := 0
+	for _, num := range data {
+		curr, err := strconv.Atoi(num)
+		if err != nil {
+			fmt.Println("Error parsing input")
+			return -1
+		}
+		newNum := 0
+		for curr != 0 {
+			digit := curr % 10
+			if newNum == 0 {
+				newNum = digit
+			} else {
+				newNum = newNum*10 + digit
+			}
+			curr /= 10
+
+		}
+		divisorSum := 0
+		limit := int(math.Sqrt(float64(newNum)))
+		for i := 1; i <= limit; i++ {
+			if newNum%i != 0 {
+				continue
+			}
+			divisor := newNum / i
+			if divisor != i {
+				divisorSum += divisor
+			}
+			divisorSum += i
+		}
+		sum += divisorSum
+	}
+
+	return sum
 }
--- learning/01-coding/02-elemental-agorithms/20-divisor-sum-mirror/solution_test.go
+++ learning/01-coding/02-elemental-agorithms/20-divisor-sum-mirror/solution_test.go
@@ -5,10 +5,21 @@ import "testing"
 func TestSuite(t *testing.T) {
 
 	data := []struct {
-		Input    int
+		Input    string
 		Expected int
 	}{
-		{Input: 0, Expected: 0},
+		{Input: "12 23 30", Expected: 99},
+		{Input: "1", Expected: 1},                            // smallest possible input
+		{Input: "100", Expected: 1},                          // trailing zeros: mirror(100) = 1
+		{Input: "10 25", Expected: 99},                       // mirror(10)=1, mirror(25)=52
+		{Input: "1 2 3 4", Expected: 15},                     // several tiny numbers
+		{Input: "121", Expected: 133},                        // palindrome: mirror(121) = 121
+		{Input: "999999999", Expected: 1534205464},           // max-length value, no trailing zeros
+		{Input: "123456789", Expected: 1515470502},           // mirror flips order completely
+		{Input: "7 70 700", Expected: 24},                    // increasing trailing zeros
+		{Input: "999999999 1 1000000", Expected: 1534205466}, // mix of max value and many zeros
+		{Input: "13 31 17 71", Expected: 136},                // mirrored pairs of each other
+		{Input: "500000000", Expected: 6},                    // mirror(500000000) = 5
 	}
 
 	for _, th := range data {
